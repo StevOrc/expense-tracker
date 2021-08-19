82e2e342c127848b73d17b8ece23107216f1439d
Steven ORCUN <steven.orcun@niji.fr>
2021-08-19 17:53:52 +0200

    logic implemented for balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 function Balance() {
-  return <div>Balance</div>;
+  const { transactions } = useContext(GlobalContext);
+  const total = transactions.reduce((acc, el) => {
+    return (acc += el.amount);
+  }, 0);
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1> {total} $</h1>
+    </>
+  );
 }
 
 export default Balance;
