121086bd0998459ac09799d5c98dc54c4db26928
Steven ORCUN <steven.orcun@niji.fr>
2021-08-20 19:33:19 +0200

    transaction list done

--- src/components/transactions/Transaction.jsx
+++ src/components/transactions/Transaction.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 
-function Transaction() {
-  return <div>Transaction !</div>;
+function Transaction({ transaction }) {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const sign = transaction.amount < 0 ? "" : "+";
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}
+      <span>
+        {sign}
+        {transaction.amount} $
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
 }
 
 export default Transaction;
